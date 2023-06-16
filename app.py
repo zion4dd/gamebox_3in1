7f9513c17885554b998a62872b8ba526cb4658aa
zion4d <zion4d@gmail.com>
2023-06-16 23:18:50 +0300

    droid_iq

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import customtkinter
 from gamefield import GameField
-from random import randint
+from random import randint, choice
+import itertools
+from time import sleep
 
-DEBUG = True
+DEBUG = True #FIXME
 SIZE = 10
 
 class App(customtkinter.CTk):
@@ -16,6 +18,8 @@ class App(customtkinter.CTk):
         self._size = size
         self._fhuman = field_human
         self._fdroid = field_droid
+        self.fire_cells = [(x, y) for x in range(self._size) for y in range(self._size)]
+        self.fire_next = set()
 
         # ============ create frames (2x1 +1) ============
         self.frame_left = customtkinter.CTkFrame(master=self,
@@ -46,8 +50,9 @@ class App(customtkinter.CTk):
             for j in range(self._size):
                 self.field_left[i][j] = customtkinter.CTkButton(
                                     master=self.frame_left,
-                                    text='.',
+                                    text='',
                                     width=28,
+                                    border_color='white',
                                     fg_color=self.cell_view_human(i, j),
                                     command=lambda row=i, column=j: self.hit(row, column))
                 self.field_left[i][j].grid(row=i, column=j)
@@ -58,7 +63,7 @@ class App(customtkinter.CTk):
             for j in range(self._size):
                 self.field_right[i][j] = customtkinter.CTkButton(
                                     master=self.frame_right,
-                                    text='.',
+                                    text='',
                                     width=28,
                                     fg_color=self.cell_view_droid(i, j),
                                     command=lambda row=i, column=j: self.hit(row, column))
@@ -84,13 +89,14 @@ class App(customtkinter.CTk):
         if self._fdroid.get_field()[i][j] == 3:
             return 'black'
         
-    def test(self):
-        self._fhuman.move_ships()
-        self._fdroid.move_ships()
-        self._fhuman.show()
-        print()
-        self._fdroid.show()
-        self.redraw()
+    def flash(self, i, j):
+        # self.field_left[i][j].configure(text='+')
+        self.field_left[i][j].configure(border_width=4, text='+')
+        self.update()
+        sleep(0.8)
+        # self.field_left[i][j].configure(text='')
+        self.field_left[i][j].configure(border_width=0, text='')
+
 
     def redraw(self):
         for i in range(self._size):
@@ -107,12 +113,55 @@ class App(customtkinter.CTk):
         self.redraw()
 
     def droid(self):
-        x = randint(0, self._size - 1)
-        y = randint(0, self._size - 1)
-        print(x, y)
+        if self.fire_next:
+            x, y = choice(tuple(self.fire_next))
+            self.fire_next.remove((x, y))
+        else:
+            x, y = choice(self.fire_cells)
+        print(x, y) #test
+        self.flash(x, y)
         if self._fhuman.get_field()[x][y] == 1:
             self._fhuman.hit(y, x)
-        
+
+            self.droid_helper()
+
+    def droid_helper(self):
+        def add_next(x, y):
+            if min(x, y) >= 0 and max(x, y) < self._size:
+                self.fire_next.add((x, y))
+
+        fhuman = self._fhuman.get_field()
+        for i in range(self._size):
+            for j in range(self._size):
+                
+                if fhuman[i][j] == 2:
+                    for d in (-1, 1):
+                        x, y = i + d, j
+                        add_next(x, y)
+                        x, y = i, j + d
+                        add_next(x, y)
+                    print(self.fire_next) #test
+                    # self.fire_next.remove((i, j))
+                    # self.fire_cells.remove((i, j))
+                
+                if fhuman[i][j] == 3:
+                    for a, b in itertools.product([-1,0,1], repeat=2):
+                        try:
+                            self.fire_cells.remove((i+a, j+b))
+                            self.fire_next.remove((i+a, j+b))
+                        except:
+                            continue
+
+    def test(self):
+        print(self.fire_next)
+        print(self.fire_cells)
+        # self._fhuman.move_ships()
+        # self._fdroid.move_ships()
+        # self._fhuman.show()
+        # print()
+        # self._fdroid.show()
+        # self.redraw()
+
 
 if __name__ == "__main__":
     print('go')
@@ -124,3 +173,10 @@ if __name__ == "__main__":
     app = App(field_human, field_droid, size=SIZE)
     app.mainloop()
 
+
+    # def droid(self):
+    #     x = randint(0, self._size - 1)
+    #     y = randint(0, self._size - 1)
+    #     print(x, y)
+    #     if self._fhuman.get_field()[x][y] == 1:
+    #         self._fhuman.hit(y, x)
