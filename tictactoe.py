3b069395efc148eac980c2ad6d0263883841f44f
zion4d <zion4d@gmail.com>
2023-06-23 13:34:10 +0300

    refactoring

--- tictactoe.py
+++ tictactoe.py
@@ -4,10 +4,10 @@ from tictactoe_field import GameField
 import os, sys
 
 
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("green")
+customtkinter.set_appearance_mode("system")
+# customtkinter.set_default_color_theme("green")
 
-DEV = False
+DEV = True
 
 class App(customtkinter.CTk):
     def __init__(self, gamefield):
@@ -18,24 +18,26 @@ class App(customtkinter.CTk):
 
         self.resizable(False, False)
         self.geometry("+400+70")
+        self.get_ico()
 
-        self.game = gamefield
+        self.gamefield = gamefield
 
         self.frame = customtkinter.CTkFrame(master=self)
         self.frame.grid(padx=30, pady=30)
 
-        self.field = [[0] * 3 for _ in range(3)]
-        for i in range(3):
-            for j in range(3):
-                self.field[i][j] = customtkinter.CTkButton(master=self.frame,
-                                text=self.cell_value(i, j),
+        self.buttons = [[0] * 3 for _ in range(3)]
+        for r in range(3):
+            for c in range(3):
+                self.buttons[r][c] = customtkinter.CTkButton(master=self.frame,
+                                text=self.cell_value((r, c)),
                                 width=70,
                                 height=70,
-                                font=('Verdana', 30),
-                                command=lambda row=i, column=j: self.go(row, column))
-                self.field[i][j].grid(row=i, column=j)
-
-        self.get_ico()
+                                fg_color=["#DC143C", "#DC143C"],
+                                hover_color=["#A50021", "#A50021"],
+                                text_color='black',
+                                font=('Verdana', 36),
+                                command=lambda row=r, column=c: self.go((row, column)))
+                self.buttons[r][c].grid(row=r, column=c)
 
     def get_ico(self):
         try:
@@ -45,32 +47,29 @@ class App(customtkinter.CTk):
             
             path = os.path.join(sys._MEIPASS, self.icofile)
             self.iconbitmap(default=path)
-        except:
-            pass
+        except: pass
 
-    def cell_value(self, r, c):
-        if self.game[r, c] == 0:
-            return ''
-        if self.game[r, c] == 1:
-            return 'X'
-        if self.game[r, c] == 2:
-            return 'O'
+    def cell_value(self, coords: tuple):
+        match self.gamefield[coords]:
+            case 0: return ''
+            case 1: return 'X'
+            case 2: return 'O'
 
     def redraw(self):
-        for i in range(3):
-            for j in range(3):
-                self.field[i][j].configure(text=self.cell_value(i, j))
+        for r in range(3):
+            for c in range(3):
+                self.buttons[r][c].configure(text=self.cell_value((r, c)))
         self.update()
     
-    def go(self, r, c):
-        if self.game.human(r, c):
+    def go(self, coords: tuple):
+        if self.gamefield.human(coords):
             if self.check_win():
-                self.game.droid()
+                self.gamefield.droid()
                 self.check_win()
 
     def check_win(self):
         self.redraw()
-        match self.game.win:
+        match self.gamefield.win:
             case 0: return True
             case 1: tkinter.messagebox.showinfo(message='Human WIN!')
             case 2: tkinter.messagebox.showinfo(message='Droid WIN!')
@@ -78,7 +77,7 @@ class App(customtkinter.CTk):
         self.restart()
 
     def restart(self):
-        self.game.init()
+        self.gamefield.init()
         self.redraw()
 
 
--- tictactoe_field.py
+++ tictactoe_field.py
@@ -1,98 +1,90 @@
 from random import choice
 from itertools import product
 
-class Cell:
-    def __init__(self, value=0):
-        self.value = value
-    
-    def __bool__(self):
-        return self.value == 0
-
 
 class GameField:
-    FREE_CELL = 0
+    FREE = 0
     HUMAN_X = 1
     DROID_O = 2
+    DRAW = 3
 
     def __init__(self):
-        self.field = tuple(tuple(Cell() for _ in range(3)) for _ in range(3))
-        self.__win = 0  # 1 - human, 2 - droid, 3 - draw
+        self.field = [[self.FREE for _ in range(3)] for _ in range(3)]
+        self.__win = self.FREE  # 1 - human, 2 - droid, 3 - draw
         self.__cells = list(product((0,1,2), repeat=2))
     
     @property
     def win(self):
         return self.__win
     
-    def __getitem__(self, item):
-        r, c = item
-        return self.field[r][c].value # return cell value (0|1|2)
+    def __getitem__(self, coords: tuple):
+        r, c = coords
+        return self.field[r][c] # (0 | 1 | 2)
 
-    def __setitem__(self, key, value):
-        r, c = key
-        self.field[r][c].value = value
-        self.check_win(r, c, value)
+    def __setitem__(self, coords: tuple, value: int):
+        r, c = coords
+        self.field[r][c] = value
+        self.__cells.remove(coords)
+        self.check_win(coords, value)
 
     def init(self):
         self.__init__()
-        if choice((0, 1)):
+        if choice((True, False)):
             self.droid()
     
-    def check_win(self, r, c, value):
-        row = all((x.value == value for x in self.field[r]))  # check row
-        col = all((x[c].value == value for x in self.field))  # check column
+    def check_win(self, coords: tuple, value: int):
+        r, c = coords
+        row = all((x == value for x in self.field[r]))  # check 1 row
+        col = all((x[c] == value for x in self.field))  # check 1 column
+        
         diag1 = diag2 = False
         if r == c:  # diagonal
-            diag1 = all((self.field[i][i].value == value for i in range(3)))
+            diag1 = all((self[i, i] == value for i in range(3)))
         if (r + c) == 2:  # antidiagonal
-            diag2 = all((self.field[i][-i+2].value == value for i in range(3)))
+            diag2 = all((self[i, 2 - i] == value for i in range(3)))
 
         if any((row, col, diag1, diag2)):
-            if value == self.HUMAN_X:
-                self.__win = 1
-            else:
-                self.__win = 2
+            self.__win = value
             return
-        # draw check:
-        if not any((x.value == self.FREE_CELL for row in self.field for x in row)):
-            self.__win = 3
+        
+        if not self.__cells: # check draw
+            self.__win = self.DRAW
 
-    def human(self, r, c):
-        if self.field[r][c]:
-            self[r, c] = self.HUMAN_X
-            self.__cells.remove((r, c))
+    def human(self, coords: tuple):
+        if self[coords] == self.FREE:
+            self[coords] = self.HUMAN_X
             return True
 
     def droid(self):
-        coords = self.droid_iq(2)
+        coords = self.droid_iq(self.DROID_O)
         if not coords:
-            coords = self.droid_iq(1)
+            coords = self.droid_iq(self.HUMAN_X)
             if not coords:
                 coords = choice(self.__cells)
-        self[*coords] = self.DROID_O
-        self.__cells.remove(coords)
+        self[coords] = self.DROID_O
 
-    def droid_iq(self, xo):
-        d1, d2 = [], []
+    def droid_iq(self, xo: int):
+        diag1, diag2 = [], []
         for r in range(3):
             row, col = [], []
             for c in range(3):
                 if r == c: # diagonal
-                    d1.append(self[r, c])
+                    diag1.append(self[r, c])
                 if r + c == 2: # antidiagonal
-                    d2.append(self[r, c])
+                    diag2.append(self[r, c])
                 row.append(self[r, c])
                 col.append(self[c, r])
-            if row.count(xo) == 2 and 0 in row:
-                return r, row.index(0)
+            if row.count(xo) == 2 and self.FREE in row:
+                return r, row.index(self.FREE)
             
-            if col.count(xo) == 2 and 0 in col:
-                return col.index(0), r
+            if col.count(xo) == 2 and self.FREE in col:
+                return col.index(self.FREE), r
             
-        if d1.count(xo) == 2 and 0 in d1:
-            return d1.index(0), d1.index(0)
+        if diag1.count(xo) == 2 and self.FREE in diag1:
+            return diag1.index(self.FREE), diag1.index(self.FREE)
         
-        if d2.count(xo) == 2 and 0 in d2:
-            return d2.index(0), 2 - d2.index(0)
+        if diag2.count(xo) == 2 and self.FREE in diag2:
+            return diag2.index(self.FREE), 2 - diag2.index(self.FREE)
         
         return False
 
